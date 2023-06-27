d976244f51e9aa4d987501a572fc8c63b6fb8b94
엄인국 <eom-inguk@eom-ingug-ui-MacBookPro.local>
2023-06-27 22:55:53 +0900

    Initial commit

---
+++ src/test/java/hello/core/CoreApplicationTests.java
@@ -0,0 +1,12 @@
+package hello.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = MemberApp.class)
+public class CoreApplicationTests {
+
+    @Test
+    void contextLoads(){
+    }
+}
