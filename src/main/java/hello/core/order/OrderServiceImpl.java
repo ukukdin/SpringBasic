c77c9c38c5d95a786dfe2bc55df6833288ea5145
eom-inguk <eum714211@gmail.com>
2023-06-29 00:41:24 +0900

    수요일 - 관심사의 분리 와 인터페이스의 의존성 관련 코드 수정

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,21 +1,33 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPoilicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl  implements OrderService{
+    /**
+     * 인터페이스에만 의존하도록 설계를 변경했다.
+     *
+     * */
+    private final MemberRepository memberRepository;
+    private final DiscountPoilicy discountPoilicy;
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPoilicy discountPoilicy) {
+        this.memberRepository = memberRepository;
+        this.discountPoilicy = discountPoilicy;
+    }
+
+   /*
+    인터페이스에만 의존하도록 다시 설계를 하면서 이전 코드를 주석 처리했습니다.
     private  final DiscountPoilicy discountPolicy = new FixDiscountPolicy();
+    private  final DiscountPoilicy discountPolicy = new RateDiscountPolicy();*/
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice){
         Member member = memberRepository.findById(memberId);
-        int discountPrice = discountPolicy.discount(member,itemPrice);
+        int discountPrice = discountPoilicy.discount(member, itemPrice);
 
-        return new Order(memberId,itemName,itemPrice,discountPrice );
+        return new Order(memberId,itemName,itemPrice,discountPrice);
     }
 
 }
